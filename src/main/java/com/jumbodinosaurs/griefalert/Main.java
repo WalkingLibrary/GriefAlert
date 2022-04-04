9bd886d745fcd9d1b258dd8d0f20a52d646a0dea
WalkingLibrary <jumboofawarrior@gmail.com>
2022-04-04 13:05:25 -0600

    - Defined Events - added Plugin .yml - Adjustments to build.gradle - added Spigot dependencies
    Signed-off-by: WalkingLibrary <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/griefalert/Main.java
+++ src/main/java/com/jumbodinosaurs/griefalert/Main.java
@@ -1,7 +1,41 @@
 package com.jumbodinosaurs.griefalert;
 
-public class Main {
+import com.jumbodinosaurs.devlib.log.LogManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin implements Listener
+{
 
+    @EventHandler
+    public static void onBlockBreak(BlockBreakEvent event)
+    {
+        LogManager.consoleLogger.info(event.getPlayer().getDisplayName());
+    }
 
+    @EventHandler
+    public static void onBlockPlace(BlockPlaceEvent event)
+    {
+
+        LogManager.consoleLogger.info(event.getPlayer().getDisplayName());
+    }
+
+    @Override
+    public void onDisable()
+    {
+        super.onDisable();
+        LogManager.consoleLogger.info("Stopping Grief Alert");
+    }
+
+    @Override
+    public void onEnable()
+    {
+        super.onEnable();
+        getServer().getPluginManager().registerEvents(this, this);
+        LogManager.consoleLogger.info("Starting Grief Alert");
+
+    }
 }
